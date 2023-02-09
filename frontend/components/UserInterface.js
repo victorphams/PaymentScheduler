2ad4f6cbc095146c11c324a5664c305224d89490
Thomas Nguyen <ttn@ou.edu>
2023-02-09 13:35:37 -0600

    New UI

--- frontend/components/UserInterface.js
+++ frontend/components/UserInterface.js
@@ -16,14 +16,39 @@ export default function UserInterface() {
   const [endDate, setEndDate] = useState(null);
 
   const [country, setCountry] = React.useState("");
+  const [frequency, setFrequency] = React.useState("");
+  const [businessDayRule, setBusinessDayRule] = React.useState("");
 
-  const handleChange = (event) => {
+  const [endMonthRule, setEndMonthRule] = React.useState(false);
+
+  const handleEndMonthRule = () => {
+    setEndMonthRule(!endMonthRule);
+  };
+
+  const handleChangeCountry = (event) => {
     setCountry(event.target.value);
   };
 
+  const handleChangeFrequency = (event) => {
+    setFrequency(event.target.value);
+  };
+
+  const handleChangeBusinessDayRule = (event) => {
+    setBusinessDayRule(event.target.value);
+  };
+
+  const handleClick = () => {
+    console.log("Country: " + country 
+    + "\n" + "Start Date: " + startDate
+    + "\n" + "End Date: " + endDate
+    + "\n" + "Frequency: " + frequency
+    + "\n" + "Business Day Rule: " + businessDayRule
+    + "\n" + "End of Month Rule: " + endMonthRule)
+  }
+
   return (
     <Box sx={{ width: "60%" }}>
-      <h1>Group 2</h1>
+      <h1>Elmo 4 Lyfe</h1>
 
       <Box sx={{ display: "flex", flexDirection: "row" }}>
         <Box
@@ -36,7 +61,7 @@ export default function UserInterface() {
               id="demo-simple-select"
               value={country}
               label="Country"
-              onChange={handleChange}
+              onChange={handleChangeCountry}
             >
               <MenuItem value={"United States"}>United States</MenuItem>
               <MenuItem value={"Brazil"}>Brazil</MenuItem>
@@ -59,11 +84,58 @@ export default function UserInterface() {
             placeholderText="End Date"
           />
 
-          <button>Units of Time</button>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Frequency</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={frequency}
+              label="Frequency"
+              onChange={handleChangeFrequency}
+            >
+              <MenuItem value={"Annual"}>Annual</MenuItem>
+              <MenuItem value={"Semiannual"}>Semiannual</MenuItem>
+              <MenuItem value={"Quarterly"}>Quarterly</MenuItem>
+              <MenuItem value={"Bimonthly"}>Bimonthly</MenuItem>
+              <MenuItem value={"Monthly"}>Monthly</MenuItem>
+              <MenuItem value={"Weekly"}>Weekly</MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              Business Day Rule
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={businessDayRule}
+              label="Business Day Rule"
+              onChange={handleChangeBusinessDayRule}
+            >
+              <MenuItem value={"Following"}>Following</MenuItem>
+              <MenuItem value={"Preceding"}>Preceding</MenuItem>
+              <MenuItem value={"Modified Following"}>
+                Modified Following
+              </MenuItem>
+              <MenuItem value={"Modified Preceding"}>
+                Modified Preceding
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <label>
+            <input
+              type="checkbox"
+              checked={endMonthRule}
+              onChange={handleEndMonthRule}
+            ></input>
+            End Month Rule
+          </label>
         </Box>
 
         <Box sx={{ marginLeft: "auto" }}>
-          <button>Search!</button>
+          <button onClick={handleClick}>Search!</button>
         </Box>
       </Box>
 
