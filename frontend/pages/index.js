447fc3a9b313e51043a4ace4680e4054a6b75bdd
Thomas Nguyen <ttn@ou.edu>
2023-04-06 14:59:06 -0500

    Fix bug with <title> tag

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>MSCI: Payment Scheduler/title>
+        <title>MSCI: Payment Scheduler</title>
         <meta name="description" content="MSCI: Payment Scheduler" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
